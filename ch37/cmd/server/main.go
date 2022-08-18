7f20a0d114e8d048e0f912940a9de9d00052a76a
442168402@qq.com <442168402@qq.com>
2022-08-18 11:20:52 +0800

    feat: change

--- ch37/cmd/server/main.go
+++ ch37/cmd/server/main.go
@@ -1,22 +1,39 @@
 package main
 
 import (
-	"flag"
+	"context"
 	"log"
-	pb "github.com/henrysworld/study2022go/ch37/cmd/helloworld"
+	"net"
 
+	pb "github.com/henrysworld/study2022go/ch37/cmd/helloworld"
 	"google.golang.org/grpc"
 )
 
 var (
-	port = flag.Int("port", 50051, "The server port")
+	port = ":50052"
 )
 
-type server struct{
-	pb.UnimplementedG
+type server struct {
+	pb.UnimplementedGreeterServer
 }
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloResponse{Message: "Hello " + in.GetName()}, nil
+}
+
 func main() {
-	log.Printf()
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed t o serve: v%", err)
+	}
+
 }
 
 // func main() {
